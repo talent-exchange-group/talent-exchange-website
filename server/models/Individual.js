ad792fde0d1d2bbaf3f85f6a83925a65bc763905
Jimmy Gong <gong.jim@gmail.com>
2015-05-27 16:33:03 -0700

    Adds skill join tables

--- server/models/Individual.js
+++ server/models/Individual.js
@@ -1,10 +1,17 @@
 var Sequelize = require('sequelize');
 var db = require('../db');
+var Skill = require('./Skill');
 
 var Individual = db.define('Individual', {
   id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
   name: Sequelize.STRING
 });
+
+IndividualSkill = db.define('IndividualSkill', {
+  id: { type: Sequelize.INTEGER, autoIncrement true, primaryKey: true }
+});
+Individual.hasMany(Skill, { through: IndividualSkill });
+Skill.hasMany(Individual, { through: IndividualSkill });
 
 /**
  * Create table if doesn't exist
--- server/models/Organization.js
+++ server/models/Organization.js
@@ -1,6 +1,7 @@
 var Sequelize = require('sequelize');
 var db = require('../db');
 var Location = require('./Location');
+var Skill = require('./Skill');
 
 var Organization = db.define('Organization', {
   id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
@@ -10,6 +11,12 @@ var Organization = db.define('Organization', {
 });
 
 Organization.belongsTo(Location, { foreignKey: 'location_id'});
+
+OrganizationSkill = db.define('OrganizationSkill', {
+  id: { type: Sequelize.INTEGER, autoIncrement true, primaryKey: true }
+});
+Organization.hasMany(Skill, { through: OrganizationSkill });
+Skill.hasMany(Organization, { through: OrganizationSkill });
 
 /**
  * Create table if doesn't exist
