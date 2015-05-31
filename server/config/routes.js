a7baf846fd768191e769a4da3ae3c0ad9e84c804
Jimmy Gong <gong.jim@gmail.com>
2015-05-31 13:10:29 -0700

    Adds location API routing

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,7 +2,7 @@ var AuthController = require('../controllers/AuthController');
 var IndController = require('../controllers/IndController');
 var OrgController = require('../controllers/OrgController');
 var SkillController = require('../controllers/SkillController');
-
+var LocController = require('../controllers/LocController');
 function router(app, passport){
   app.get('/', function(req, res){
     return res.sendStatus(200);
@@ -40,6 +40,32 @@ function router(app, passport){
     var email = req.body.email;
     return IndController.remove(email, function(removedInd){
       return res.send(removedInd);
+    });
+  });
+  /**
+   * LOCATION API
+   */ 
+  app.get('/api/location/*', function(req, res){
+    var param = req.params[0].split('=');
+    if(param[0] === 'all'){
+      return LocController.getAll(function(locations){
+        return res.send(locations);
+      });
+    }
+    return LocController.getId(function(location){
+      return res.send(location);
+    });
+  });
+  app.post('/api/location/create', function(req, res){
+    var location = req.body.location;
+    return LocController.add(location, function(addedLoc){
+      return res.send(addedLoc);
+    });
+  });
+  app.post('/api/location/remove', function(req, res){
+    var location = req.body.location;
+    return LocController.remove(location, function(removedLoc){
+      return res.send(removedLoc);
     });
   });
   /**
