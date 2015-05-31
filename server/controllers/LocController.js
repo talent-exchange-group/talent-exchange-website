b521beeb43d425addd0bc7a8674c734abdb0efce
Jimmy Gong <gong.jim@gmail.com>
2015-05-31 13:02:57 -0700

    Scaffold Location Controller

---
+++ server/controllers/LocController.js
@@ -0,0 +1,19 @@
+var Location = require('../models/Location');
+var utils = require('../helpers/utils');
+
+var controller = {
+  getId: function(locName, callback){
+
+  },
+  getAll: function(callback){
+
+  },
+  add: function(locName){
+
+  },
+  remove: function(locName){
+
+  }
+};
+
+module.exports = controller;
